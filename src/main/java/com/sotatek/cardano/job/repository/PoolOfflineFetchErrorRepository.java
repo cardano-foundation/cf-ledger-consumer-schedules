99110c41ec29346b7ecb62b49b401ab0d7ce42f6
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-05-24 23:06:48 +0700

    chore: remove unused

--- src/main/java/com/sotatek/cardano/job/repository/PoolOfflineFetchErrorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sotatek.cardano.job.repository;
-
-import com.sotatek.cardano.common.entity.PoolOfflineFetchError;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PoolOfflineFetchErrorRepository extends JpaRepository<PoolOfflineFetchError, Long> {
-
-}
