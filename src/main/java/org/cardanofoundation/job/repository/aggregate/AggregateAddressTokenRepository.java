8189937fe0a9a2312806665aa7f2c17464992672
Sotatek-NamNguyen15 <nam.nguyen15@sotatek.com>
2023-06-12 17:39:41 +0700

    fix: [MET-1138] update format code with spotless

--- src/main/java/org/cardanofoundation/job/repository/aggregate/AggregateAddressTokenRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/aggregate/AggregateAddressTokenRepository.java
@@ -1,33 +1,36 @@
 package org.cardanofoundation.job.repository.aggregate;
 
-import org.cardanofoundation.explorer.consumercommon.entity.aggregation.AggregateAddressToken;
+import java.sql.Timestamp;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
+import org.cardanofoundation.explorer.consumercommon.entity.aggregation.AggregateAddressToken;
 
-public interface AggregateAddressTokenRepository extends JpaRepository<AggregateAddressToken, Long> {
+public interface AggregateAddressTokenRepository
+    extends JpaRepository<AggregateAddressToken, Long> {
 
   @Transactional
   @Modifying
   @Query(
-      value = "insert into agg_address_token (ident, balance, day) "
-          + "SELECT addt.ident as ident, "
-          + "       sum(addt.balance) as sum_balance, "
-          + "       date_trunc('day', b.time) as time_agg "
-          + "FROM address_token addt "
-          + "inner join multi_asset ma on addt.ident = ma.id "
-          + "inner join tx t on addt.tx_id = t.id "
-          + "inner join block b on t.block_id = b.id "
-          + "where b.time >= :startOfDay "
-          + "and b.time <= :endOfDay "
-          + "and b.tx_count > 0 and addt.balance > 0 "
-          + "GROUP BY addt.ident, time_agg "
-          + "order by time_agg",
+      value =
+          "insert into agg_address_token (ident, balance, day) "
+              + "SELECT addt.ident as ident, "
+              + "       sum(addt.balance) as sum_balance, "
+              + "       date_trunc('day', b.time) as time_agg "
+              + "FROM address_token addt "
+              + "inner join multi_asset ma on addt.ident = ma.id "
+              + "inner join tx t on addt.tx_id = t.id "
+              + "inner join block b on t.block_id = b.id "
+              + "where b.time >= :startOfDay "
+              + "and b.time <= :endOfDay "
+              + "and b.tx_count > 0 and addt.balance > 0 "
+              + "GROUP BY addt.ident, time_agg "
+              + "order by time_agg",
       nativeQuery = true)
-  void insertDataForDay(@Param("startOfDay") Timestamp startOfDay,
-                        @Param("endOfDay") Timestamp endOfDay);
+  void insertDataForDay(
+      @Param("startOfDay") Timestamp startOfDay, @Param("endOfDay") Timestamp endOfDay);
 }
