6193b2f832b65b79fbc4b19d7a180be49c21c987
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-07-26 23:33:07 +0700

    feat: MET-1488 store logo of token to storage

--- src/main/java/org/cardanofoundation/job/config/S3Configuration.java
+++ src/main/java/org/cardanofoundation/job/config/S3Configuration.java
@@ -1,36 +1,65 @@
 package org.cardanofoundation.job.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import java.util.List;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.cardanofoundation.job.config.properties.S3Properties;
+import org.cardanofoundation.job.config.properties.S3Properties.S3Config;
 
 @Configuration
-public class S3Configuration {
+public class S3Configuration implements BeanFactoryAware {
 
-  @Bean
-  @Profile("minio")
-  public AmazonS3 amazonS3Clone(
-      @Value("${cloud.aws.s3.endpoint}") String serviceEndpoint,
-      @Value("${cloud.aws.s3.path-style.enabled}") Boolean pathStyleAccessEnabled,
-      @Value("${cloud.aws.credentials.access-key}") String accessKey,
-      @Value("${cloud.aws.credentials.secret-key}") String secretKey,
-      @Value("${cloud.aws.region.static}") String region,
-      @Value("${cloud.aws.s3.bucket.name}") String bucketName) {
-    final AmazonS3 amazonS3 =
-        AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(
-                new AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
-            .withPathStyleAccessEnabled(pathStyleAccessEnabled)
-            .withCredentials(
-                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-            .build();
+  private static final String S3_PROFILE = "s3";
+  private static final String MINIO_PROFILE = "minio";
+  private final S3Properties s3Properties;
+  private BeanFactory beanFactory;
+
+  public S3Configuration(S3Properties s3Properties) {this.s3Properties = s3Properties;}
+
+  @Override
+  public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+    this.beanFactory = beanFactory;
+  }
+
+  @PostConstruct
+  public void onPostConstruct() {
+    ConfigurableBeanFactory configurableBeanFactory = (ConfigurableBeanFactory) beanFactory;
+    List<S3Config> s3Beans = s3Properties.getS3Configs();
+    for (S3Config s3Bean : s3Beans) {
+      // setup beans programmatically
+      AmazonS3 amazonS3 = switch (s3Bean.getProfile()) {
+        case S3_PROFILE ->
+            buildAmazonS3(s3Bean.getAccessKey(), s3Bean.getSecretKey(), s3Bean.getRegion());
+        case MINIO_PROFILE ->
+            buildAmazonS3Clone(s3Bean.getEndpoint(), s3Bean.getAccessKey(), s3Bean.getSecretKey(),
+                               s3Bean.getRegion(), s3Bean.getBucket());
+        default -> throw new IllegalArgumentException("Invalid profile");
+      };
+      configurableBeanFactory.registerSingleton(s3Bean.getBeanName(), amazonS3);
+    }
+  }
+
+  private AmazonS3 buildAmazonS3Clone(String serviceEndpoint, String accessKey, String secretKey,
+                                      String region, String bucketName) {
+    final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+        .withEndpointConfiguration(
+            new AwsClientBuilder.EndpointConfiguration(serviceEndpoint, region))
+        .withPathStyleAccessEnabled(true)
+        .withCredentials(
+            new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+        .build();
 
     if (!amazonS3.doesBucketExistV2(bucketName)) {
       amazonS3.createBucket(bucketName);
@@ -38,23 +67,11 @@ public class S3Configuration {
     return amazonS3;
   }
 
-  @Bean
-  @Profile("s3")
-  public AmazonS3 amazonS3(
-      @Value("${cloud.aws.credentials.access-key}") String accessKey,
-      @Value("${cloud.aws.credentials.secret-key}") String secretKey,
-      @Value("${cloud.aws.region.static}") String region,
-      @Value("${cloud.aws.s3.bucket.name}") String bucketName) {
-    final AmazonS3 amazonS3 =
-        AmazonS3ClientBuilder.standard()
-            .withRegion(region)
-            .withCredentials(
-                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
-            .build();
-
-    if (!amazonS3.doesBucketExistV2(bucketName)) {
-      amazonS3.createBucket(bucketName);
-    }
-    return amazonS3;
+  private AmazonS3 buildAmazonS3(String accessKey, String secretKey, String region) {
+    return AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .withCredentials(
+            new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+        .build();
   }
 }
---
+++ src/main/java/org/cardanofoundation/job/schedules/AssetMetadataSchedule.java
@@ -0,0 +1,235 @@
+package org.cardanofoundation.job.schedules;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.xml.bind.DatatypeConverter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bloxbean.cardano.client.crypto.Blake2bUtil;
+import com.bloxbean.cardano.client.util.HexUtil;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cardanofoundation.job.service.StorageService;
+import org.cardanofoundation.job.util.DataUtil;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import org.cardanofoundation.explorer.consumercommon.entity.AssetMetadata;
+import org.cardanofoundation.job.dto.AssetMetadataDTO;
+import org.cardanofoundation.job.mapper.AssetMedataMapper;
+import org.cardanofoundation.job.repository.AssetMetadataRepository;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@ConditionalOnProperty(value = "jobs.meta-data.enabled", matchIfMissing = true, havingValue = "true")
+public class AssetMetadataSchedule {
+
+  private final AssetMetadataRepository assetMetadataRepository;
+  private final AssetMedataMapper assetMedataMapper;
+
+  @Autowired
+  @Qualifier("storageTokenServiceImpl")
+  private StorageService storageService;
+
+  @Value("${token.metadata.url}")
+  private String url;
+  @Value("${token.metadata.folder}")
+  private String metadataFolder;
+  @Value("${application.network}")
+  private String network;
+  @Value("${clouds.s3Configs[1].endpoint}")
+  private String endpoint;
+  @Value("${clouds.s3Configs[1].bucket}")
+  private String bucketName;
+
+  @Transactional
+  @Scheduled(fixedRate = 2000000, initialDelay = 2000)
+  public void syncMetaData() throws IOException, GitAPIException {
+    String pathFolder = cloneTokenMetadataRepo();
+    List<AssetMetadataDTO> assetMetadataList = readTokenMetadata(pathFolder);
+    saveTokenMetada(assetMetadataList);
+  }
+
+  /**
+   * Save token metadata to database and upload logo to s3
+   *
+   * @param assetMetadataDTOList list token metadata
+   */
+  private void saveTokenMetada(List<AssetMetadataDTO> assetMetadataDTOList) {
+    log.info("Save {} token metadata to database", assetMetadataDTOList.size());
+    var currentTime = System.currentTimeMillis();
+    // Map AssetMetadataDTO need to upload logo s3
+    Map<String, AssetMetadataDTO> assetMetadataMapUpload = new HashMap<>();
+
+    // Map AssetMetadata source from database
+    Map<String, AssetMetadata> assetMetadataMapSource = assetMetadataRepository.findAll().stream()
+        .collect(Collectors.toMap(AssetMetadata::getSubject, Function.identity()));
+
+    // List AssetMetadata need to save to database
+    List<AssetMetadata> assetMetadataList = new ArrayList<>();
+
+    assetMetadataDTOList.forEach(assetMetadataDTO -> {
+      // Check if token metadata exist then update
+      boolean flagUpload = false;
+      var assetMetadataTarget = assetMedataMapper.fromDTO(assetMetadataDTO);
+      // Generate logo hash
+      var logoHash = generateLogoHash(assetMetadataDTO);
+      assetMetadataTarget.setLogoHash(logoHash);
+      if (assetMetadataMapSource.containsKey(assetMetadataDTO.getSubject())) {
+        var assetMetadataSource = assetMetadataMapSource.get(assetMetadataDTO.getSubject());
+        assetMetadataTarget.setId(assetMetadataSource.getId());
+        assetMetadataTarget.setLogo(assetMetadataSource.getLogo());
+        assetMetadataTarget.setLogoHash(assetMetadataSource.getLogoHash());
+        // Check if logo hash changed then mark flagUpload = true
+        flagUpload = !Objects.equals(assetMetadataSource.getLogoHash(),
+                                     assetMetadataTarget.getLogoHash());
+      } else {
+        // Check if logo hash not null then mark flagUpload = true
+        flagUpload = !Objects.equals(assetMetadataTarget.getLogoHash(), null);
+      }
+      // if flagUpload = true then add to assetMetadataMapUpload and set logo url
+      if (Boolean.TRUE.equals(flagUpload)) {
+        assetMetadataMapUpload.put(assetMetadataDTO.getSubject(), assetMetadataDTO);
+        assetMetadataTarget.setLogo(
+            endpoint + "/" + bucketName + "/" + network + "/" + assetMetadataDTO.getSubject());
+        assetMetadataTarget.setLogoHash(logoHash);
+      }
+      assetMetadataList.add(assetMetadataTarget);
+    });
+    log.info("Processing raw data done!! Time taken: {} ms",
+             System.currentTimeMillis() - currentTime);
+    currentTime = System.currentTimeMillis();
+
+    log.info("Uploading {} token logo to s3", assetMetadataMapUpload.size());
+    List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
+    assetMetadataMapUpload.forEach(
+        (key, value) -> completableFutures.add(CompletableFuture.runAsync(() -> {
+          try (InputStream inputStream = base64ToInputStream(value.getLogo().getValue())) {
+            storageService.uploadFile(inputStream.readAllBytes(),
+                                      network + "/" + value.getSubject());
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        })));
+    completableFutures.forEach(CompletableFuture::join);
+    log.info("Upload token logo done!! Time taken: {} ms",
+             System.currentTimeMillis() - currentTime);
+
+    assetMetadataRepository.saveAll(assetMetadataList);
+    log.info("Done save {} token metadata to database", assetMetadataList.size());
+  }
+
+  /**
+   * Go through all files in token-metadata repo and read json file to AssetMetadataDTO
+   *
+   * @param pathFolder path to token-metadata repo
+   * @return List<AssetMetadataDTO>
+   * @throws IOException
+   */
+  private List<AssetMetadataDTO> readTokenMetadata(String pathFolder) throws IOException {
+    List<AssetMetadataDTO> assetMetadataList = new ArrayList<>();
+    try (Stream<Path> paths = Files.walk(Paths.get(pathFolder + metadataFolder))) {
+      paths.filter(Files::isRegularFile).forEach(file -> {
+        try {
+          ObjectMapper mapper = new ObjectMapper();
+          Reader reader = Files.newBufferedReader(file);
+          mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+          AssetMetadataDTO assetMetadataDTO = mapper.readValue(reader, AssetMetadataDTO.class);
+          log.info("Crawl token: {}", assetMetadataDTO.getName().getValue());
+          log.info(file.getFileName().toString());
+          if (file.getFileName().toString().equals(assetMetadataDTO.getSubject().concat(".json"))
+              && assetMetadataDTO.getSubject().length() >= 56) {
+            assetMetadataList.add(assetMetadataDTO);
+          }
+          reader.close();
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+      });
+    }
+    return assetMetadataList;
+  }
+
+  /**
+   * Clone or pull token-metadata repo
+   *
+   * @return path to token-metadata repo
+   * @throws GitAPIException
+   * @throws IOException
+   */
+  private String cloneTokenMetadataRepo() throws GitAPIException, IOException {
+    String pathFolder = "./token-metadata-" + network;
+    log.info("Clone metadata repository: " + url);
+    File folder = new File(pathFolder);
+    if (!folder.exists()) {
+      Git git = Git.cloneRepository().setURI(url).setDirectory(folder).call();
+      if (git != null) {
+        log.info("Clone metadata repository done");
+      } else {
+        log.error("Clone metadata repository error");
+      }
+    } else {
+      try (Git git = Git.open(folder)) {
+        PullResult pull = git.pull().call();
+        if (pull.isSuccessful()) {
+          log.info("Pull metadata repository done");
+        } else {
+          log.error("Pull metadata repository error");
+        }
+      }
+    }
+    return pathFolder;
+  }
+
+  /**
+   * Convert base64 string to InputStream
+   *
+   * @param base64String base64 string
+   * @return InputStream
+   */
+  private InputStream base64ToInputStream(String base64String) {
+    return new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(base64String));
+  }
+
+  /**
+   * Generate logo hash from AssetMetadataDTO
+   *
+   * @param assetMetadataDTO
+   * @return null if AssetMetadataDTO.logo is null or encoded logo hash
+   */
+  private String generateLogoHash(AssetMetadataDTO assetMetadataDTO) {
+    if (DataUtil.isNullOrEmpty(assetMetadataDTO.getLogo())) {
+      return null;
+    }
+    String hash = endpoint + "/" + bucketName + "/" + network + "/" + assetMetadataDTO.getLogo().getValue();
+    return HexUtil.encodeHexString(Blake2bUtil.blake2bHash256(hash.getBytes()));
+  }
+}
