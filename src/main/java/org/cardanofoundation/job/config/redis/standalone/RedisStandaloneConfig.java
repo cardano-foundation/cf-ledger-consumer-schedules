d9306539d87498392b76db47a163d20ea7f00327
Giovanni Gargiulo <nemo83@users.noreply.github.com>
2023-08-02 08:36:06 +0100

    Fix/enable tls for standalone redis (#166)
    * fix: added tls configuratio nfor standalone
    
    * fix: fix ssl config for kafka

--- src/main/java/org/cardanofoundation/job/config/redis/standalone/RedisStandaloneConfig.java
+++ src/main/java/org/cardanofoundation/job/config/redis/standalone/RedisStandaloneConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -26,12 +27,23 @@ public class RedisStandaloneConfig {
   @Value("${spring.redis.password}")
   private String password;
 
-  @Bean(name = "lettuceConnectionFactory")
-  LettuceConnectionFactory lettuceConnectionFactory() {
-    RedisStandaloneConfiguration redisStandaloneConfiguration =
-        new RedisStandaloneConfiguration(hostname, port);
+  @Value("${spring.redis.standalone.useSsl}")
+  private boolean useSsl;
+
+  @Bean
+  RedisStandaloneConfiguration redisStandaloneConfiguration() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(hostname, port);
     redisStandaloneConfiguration.setPassword(password);
-    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    return redisStandaloneConfiguration;
+  }
+
+  @Bean(name = "lettuceConnectionFactory")
+  LettuceConnectionFactory lettuceConnectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration) {
+    if(useSsl) {
+      return new LettuceConnectionFactory(redisStandaloneConfiguration, LettuceClientConfiguration.builder().useSsl().build());
+    } else {
+      return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
   }
 
   @Bean
