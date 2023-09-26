c60015875bf27c7240f532f1143ba4ed95164d54
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-09-26 18:29:00 +0700

    feat: add live pool delegators count schedule cache

--- src/main/java/org/cardanofoundation/job/service/DelegationService.java
+++ src/main/java/org/cardanofoundation/job/service/DelegationService.java
@@ -1,6 +1,12 @@
 package org.cardanofoundation.job.service;
 
+import java.util.List;
+
+import org.cardanofoundation.job.projection.PoolCountProjection;
+
 public interface DelegationService {
 
   Integer countCurrentDelegator();
+
+  List<PoolCountProjection> getAllLivePoolDelegatorsCount();
 }
