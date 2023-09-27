23c66b00469dc7f371c60ec7634cc605f5000f66
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-09-27 14:57:00 +0700

    feat: add new PoolInfoSchedule to aggregate pool info to redis cache

--- src/main/java/org/cardanofoundation/job/schedules/DelegationSchedule.java
+++ src/main/java/org/cardanofoundation/job/schedules/DelegationSchedule.java
@@ -1,21 +1,15 @@
 package org.cardanofoundation.job.schedules;
 
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-
+import org.cardanofoundation.job.common.enumeration.RedisKey;
+import org.cardanofoundation.job.service.DelegationService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import org.cardanofoundation.job.common.enumeration.RedisKey;
-import org.cardanofoundation.job.projection.PoolCountProjection;
-import org.cardanofoundation.job.service.DelegationService;
 
 @Slf4j
 @Component
@@ -36,22 +30,6 @@ public class DelegationSchedule {
     String delegatorKey = getRedisKey(RedisKey.TOTAL_DELEGATOR.name());
     redisTemplate.opsForValue().set(delegatorKey, numberDelegator);
     log.info("Update number of delegator {} successfully", numberDelegator);
-  }
-
-  @Scheduled(fixedDelayString = "30000")
-  public void updateLivePoolDelegatorsCount(){
-    log.info("Update live pool delegators count!");
-    long startTime = System.currentTimeMillis();
-    Map<Long, Integer> livePoolDelegatorsCountMap =
-        delegatorService.getAllLivePoolDelegatorsCount()
-            .stream()
-            .collect(Collectors.
-                         toMap(PoolCountProjection::getPoolId,PoolCountProjection::getCountValue));
-
-    String livePoolDelegatorsCountKey = getRedisKey(RedisKey.POOLS_LIVE_DELEGATORS_COUNT.name());
-    redisTemplate.opsForHash().putAll(livePoolDelegatorsCountKey, livePoolDelegatorsCountMap);
-    log.info("Update live pool delegators count successfully, Time taken: {} ms",
-             System.currentTimeMillis() - startTime);
   }
 
   private String getRedisKey(String prefix) {
