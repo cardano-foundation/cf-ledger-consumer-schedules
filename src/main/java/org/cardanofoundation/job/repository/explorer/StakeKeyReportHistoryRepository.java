dc01e26b3de97e733ce619b765466cbb78b27db9
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-10-20 14:42:05 +0700

    feat: MET-1447 update report logic with multi datasource config

--- src/main/java/org/cardanofoundation/job/repository/explorer/StakeKeyReportHistoryRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/explorer/StakeKeyReportHistoryRepository.java
@@ -1,9 +1,9 @@
-package org.cardanofoundation.job.repository.ledgersync;
+package org.cardanofoundation.job.repository.explorer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import org.cardanofoundation.explorer.consumercommon.entity.StakeKeyReportHistory;
+import org.cardanofoundation.explorer.consumercommon.explorer.entity.StakeKeyReportHistory;
 
 public interface StakeKeyReportHistoryRepository
     extends JpaRepository<StakeKeyReportHistory, Long> {
