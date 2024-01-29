600fb4d4f39fae74e3b5a8273a3f0067c8e13eec
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-01-29 11:24:44 +0700

    fix: remove unused migration in ledger-sync-v2

--- src/main/resources/db/migration/ledgersync/V1_3_8__update_table_pool_offline_data.sql
+++ src/main/resources/db/migration/ledgersync/V1_3_8__update_table_pool_offline_data.sql
@@ -1,11 +1,2 @@
-TRUNCATE TABLE pool_offline_data;
-ALTER SEQUENCE pool_offline_data_id_seq RESTART;
-TRUNCATE TABLE pool_offline_fetch_error;
-ALTER SEQUENCE pool_offline_fetch_error_id_seq RESTART;
-
 CREATE INDEX IF NOT EXISTS pool_offline_fetch_error_pool_id_idx  ON pool_offline_fetch_error (pool_id);
 CREATE INDEX IF NOT EXISTS pool_offline_fetch_error_pool_id_pmr_id_idx ON pool_offline_fetch_error (pool_id,pmr_id);
-
-ALTER TABLE pool_offline_data DROP CONSTRAINT IF EXISTS unique_pool_offline_data;
-ALTER TABLE pool_offline_data DROP CONSTRAINT IF EXISTS pool_offline_data_pool_id_unique;
-ALTER TABLE pool_offline_data ADD CONSTRAINT pool_offline_data_pool_id_unique UNIQUE (pool_id);
