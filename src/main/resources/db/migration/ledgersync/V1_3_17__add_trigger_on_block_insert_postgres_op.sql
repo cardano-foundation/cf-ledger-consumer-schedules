8c6c3d1dcf0882e69a6c310b7af97b3e4cf8b119
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-02-05 15:52:10 +0700

    chore: changes notify block_id to notify block_hash

--- src/main/resources/db/migration/ledgersync/V1_3_17__add_trigger_on_block_insert_postgres_op.sql
+++ src/main/resources/db/migration/ledgersync/V1_3_17__add_trigger_on_block_insert_postgres_op.sql
@@ -1,15 +1,13 @@
 CREATE OR REPLACE FUNCTION notify_new_block_in_queue()
     RETURNS TRIGGER AS $$
-DECLARE
-    last_block_id INTEGER;
-begin
-    SELECT MAX(id) INTO last_block_id FROM block;
-    IF TG_OP = 'INSERT' AND last_block_id = new.id THEN
-        PERFORM pg_notify('new_block', NEW.id::text);
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        PERFORM pg_notify('new_block', NEW.hash::text);
     END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
 
 CREATE TRIGGER notification_block_added_in_queue
     AFTER INSERT OR UPDATE ON block
