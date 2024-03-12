99bd31480814051717ca352462b9e68866e2aade
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-03-12 10:18:54 +0700

    chore: adapt code formatter

--- src/main/java/org/cardanofoundation/job/config/redis/standalone/RedisStandaloneConfig.java
+++ src/main/java/org/cardanofoundation/job/config/redis/standalone/RedisStandaloneConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Log4j2
