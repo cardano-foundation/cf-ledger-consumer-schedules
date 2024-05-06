a84447c383d3b582f487819c300692d375ca7391
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-05-06 13:52:13 +0700

    fix: replace query for count address tx

--- src/main/resources/db/migration/ledgersync/V1_3_24__add_materialized_view_for_address_tx_count_.sql
+++ src/main/resources/db/migration/ledgersync/V1_3_24__add_materialized_view_for_address_tx_count_.sql
@@ -1,8 +1,7 @@
 DROP MATERIALIZED VIEW IF EXISTS address_tx_count;
 
 CREATE MATERIALIZED VIEW address_tx_count AS
-SELECT add.address                        AS address,
-       count(distinct ata.tx_hash)                        AS tx_count
-FROM address add
-         left join address_tx_amount ata on ata.address = add.address
-GROUP BY add.address;
+SELECT ata.address                        AS address,
+       count(distinct ata.tx_hash)        AS tx_count
+FROM address_tx_amount ata
+GROUP BY ata.address;
