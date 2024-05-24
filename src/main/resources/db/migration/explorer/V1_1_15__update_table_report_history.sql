6e6b8d7ea51d955832b739aa963c9cd8ec5e397d
Sotatek-VuongTa <tung.nguyen6@sotatek.com>
2024-05-24 14:05:29 +0700

    update: add job consume report and remove kafka

---
+++ src/main/resources/db/migration/explorer/V1_1_15__update_table_report_history.sql
@@ -0,0 +1,8 @@
+after table report_history
+    add column zone_offset integer default null;
+
+after table report_history
+    add column time_pattern string default null;
+
+after table report_history
+    add column date_format string default null;
