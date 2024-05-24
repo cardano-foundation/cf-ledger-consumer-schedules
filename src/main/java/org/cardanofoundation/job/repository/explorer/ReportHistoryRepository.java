765103417ee0ce90da3afd7412942c3750207126
Sotatek-VuongTa <tung.nguyen6@sotatek.com>
2024-05-24 14:05:29 +0700

    update: add job consume report and remove kafka

--- src/main/java/org/cardanofoundation/job/repository/explorer/ReportHistoryRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/explorer/ReportHistoryRepository.java
@@ -3,6 +3,7 @@ package org.cardanofoundation.job.repository.explorer;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.cardanofoundation.explorer.common.entity.enumeration.ReportStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,7 @@ public interface ReportHistoryRepository extends JpaRepository<ReportHistory, Lo
   @Query("SELECT r FROM ReportHistory r WHERE r.uploadedAt < :uploadedAt AND r.status <> 'EXPIRED'")
   List<ReportHistory> findNotExpiredReportHistoryByUploadedAtLessThan(
       @Param("uploadedAt") Timestamp uploadedAt);
+
+  @Query("SELECT r FROM ReportHistory r WHERE r.status = :status")
+  List<ReportHistory> findAllReportHistoryByStatus(@Param("status") ReportStatus status);
 }
--- src/main/java/org/cardanofoundation/job/schedules/ReportHistorySchedule.java
+++ src/main/java/org/cardanofoundation/job/schedules/ReportHistorySchedule.java
@@ -19,8 +19,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import org.cardanofoundation.explorer.common.entity.enumeration.ReportStatus;
+import org.cardanofoundation.explorer.common.entity.explorer.PoolReportHistory;
 import org.cardanofoundation.explorer.common.entity.explorer.ReportHistory;
+import org.cardanofoundation.explorer.common.entity.explorer.StakeKeyReportHistory;
+import org.cardanofoundation.job.repository.explorer.PoolReportHistoryRepository;
 import org.cardanofoundation.job.repository.explorer.ReportHistoryRepository;
+import org.cardanofoundation.job.repository.explorer.StakeKeyReportHistoryRepository;
+import org.cardanofoundation.job.service.PoolReportService;
+import org.cardanofoundation.job.service.StakeKeyReportService;
 import org.cardanofoundation.job.service.impl.StorageReportServiceImpl;
 
 @Component
@@ -35,6 +41,10 @@ public class ReportHistorySchedule {
 
   private final ReportHistoryRepository reportHistoryRepository;
   private final StorageReportServiceImpl storageService;
+  private final StakeKeyReportHistoryRepository stakeKeyReportHistoryRepository;
+  private final PoolReportHistoryRepository poolReportHistoryRepository;
+  private final StakeKeyReportService stakeKeyReportService;
+  private final PoolReportService poolReportService;
 
   /** Find all report history expired and delete from storage and set status to EXPIRED */
   @Scheduled(fixedRateString = "${jobs.report-history.expired.rate}", initialDelay = 3000)
@@ -61,6 +71,47 @@ public class ReportHistorySchedule {
     reportHistoryRepository.saveAllAndFlush(reportHistoryList);
     log.info(
         "Time taken to delete expired report history: {} ms",
+        System.currentTimeMillis() - currentTime);
+  }
+
+  @Scheduled(cron = "3 * * * * *", initialDelay = 3000)
+  public void consume() {
+    var currentTime = System.currentTimeMillis();
+    try {
+      List<ReportHistory> reportHistories =
+          reportHistoryRepository.findAllReportHistoryByStatus(ReportStatus.IN_PROGRESS);
+
+      reportHistories.forEach(
+          reportHistory -> {
+            switch (reportHistory.getType()) {
+              case STAKE_KEY:
+                StakeKeyReportHistory stakeKeyReportHistory =
+                    stakeKeyReportHistoryRepository.findByReportHistoryId(reportHistory.getId());
+                stakeKeyReportService.exportStakeKeyReport(
+                    stakeKeyReportHistory,
+                    reportHistory.getZoneOffset(),
+                    reportHistory.getTimePattern(),
+                    reportHistory.getDateFormat());
+                break;
+              case POOL_ID:
+                PoolReportHistory poolReportHistory =
+                    poolReportHistoryRepository.findByReportHistoryId(reportHistory.getId());
+                poolReportService.exportPoolReport(
+                    poolReportHistory,
+                    reportHistory.getZoneOffset(),
+                    reportHistory.getTimePattern(),
+                    reportHistory.getDateFormat());
+                break;
+              default:
+                break;
+            }
+          });
+    } catch (Exception e) {
+      log.error("Consume report history failure: {}", e.getMessage());
+    }
+
+    log.info(
+        "End job consume report history: {} ms",
         System.currentTimeMillis() - currentTime);
   }
 }
