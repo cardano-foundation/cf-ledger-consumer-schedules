ad47e7da728b4d855cd9d494405a52a0cd16e062
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-05-29 10:54:30 +0700

    fix: add logic check exists when updating table

--- src/main/resources/db/migration/explorer/V1_1_15__update_table_report_history.sql
+++ src/main/resources/db/migration/explorer/V1_1_15__update_table_report_history.sql
@@ -1,8 +1,8 @@
 ALTER TABLE report_history
-    ADD COLUMN zone_offset integer DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS zone_offset integer DEFAULT NULL;
 
 ALTER TABLE report_history
-    ADD COLUMN time_pattern varchar(255) DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS time_pattern varchar(255) DEFAULT NULL;
 
 ALTER TABLE report_history
-    ADD COLUMN date_format varchar(255) DEFAULT NULL;
+    ADD COLUMN IF NOT EXISTS date_format varchar(255) DEFAULT NULL;
