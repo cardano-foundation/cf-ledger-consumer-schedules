68d85b953f02a404ada4d8bd910fd4c80f0cdae3
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-05-31 08:59:13 +0100

    fix: trying to make initial token sync work

--- src/main/java/org/cardanofoundation/job/service/impl/TokenInfoServiceImpl.java
+++ src/main/java/org/cardanofoundation/job/service/impl/TokenInfoServiceImpl.java
@@ -1,30 +1,9 @@
 package org.cardanofoundation.job.service.impl;
 
-import static org.cardanofoundation.job.common.enumeration.RedisKey.AGGREGATED_CACHE;
-import static org.cardanofoundation.job.common.enumeration.RedisKey.TOTAL_TOKEN_COUNT;
-
-import java.math.BigInteger;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import org.cardanofoundation.explorer.common.entity.explorer.TokenInfo;
 import org.cardanofoundation.explorer.common.entity.explorer.TokenInfoCheckpoint;
 import org.cardanofoundation.explorer.common.entity.ledgersync.Block;
@@ -43,6 +22,26 @@ import org.cardanofoundation.job.service.TokenInfoService;
 import org.cardanofoundation.job.service.TokenInfoServiceAsync;
 import org.cardanofoundation.job.util.BatchUtils;
 import org.cardanofoundation.job.util.StreamUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.cardanofoundation.job.common.enumeration.RedisKey.AGGREGATED_CACHE;
+import static org.cardanofoundation.job.common.enumeration.RedisKey.TOTAL_TOKEN_COUNT;
 
 @Service
 @RequiredArgsConstructor
@@ -63,6 +62,9 @@ public class TokenInfoServiceImpl implements TokenInfoService {
 
   @Value("${application.network}")
   private String network;
+
+  @Value("${jobs.token-info.token-info-batch-size:1000}")
+  private Integer tokenInfoBatchSize;
 
   @Override
   @Transactional(value = "explorerTransactionManager")
@@ -99,8 +101,6 @@ public class TokenInfoServiceImpl implements TokenInfoService {
   private void initializeTokenInfoDataForFirstTime(Long maxBlockNo, Timestamp updateTime) {
     log.info("Init token info data for the first time");
 
-    List<CompletableFuture<List<TokenInfo>>> tokenInfoFutures = new ArrayList<>();
-
     var maxMultiAssetId = multiAssetRepository.getCurrentMaxIdent();
 
     if (maxMultiAssetId == null) {
@@ -108,50 +108,43 @@ public class TokenInfoServiceImpl implements TokenInfoService {
       return;
     }
 
-    // Collect the results from all CompletableFuture instances to get the list of multi-assets.
-    var multiAssetIdList =
-        LongStream.rangeClosed(1, maxMultiAssetId).boxed().collect(Collectors.toList());
-
     Timestamp time24hAgo = Timestamp.valueOf(LocalDateTime.now(ZoneOffset.UTC).minusDays(1));
 
     // Find the minimum transaction ID that occurred after the time 24 hours ago.
     Long txId = txRepository.findMinTxByAfterTime(time24hAgo).orElse(Long.MAX_VALUE);
 
     // Define the maximum batch size for processing multi-assets.
-    int multiAssetListSize = 50000;
+      var numBatches = maxMultiAssetId / tokenInfoBatchSize;
+      var reminder = maxMultiAssetId % tokenInfoBatchSize;
+
+      log.info("numBatches: {}", numBatches);
+      log.info("reminder: {}", reminder);
 
     // Process the multi-assets in batches to build token info data.
-    for (int i = 0; i < multiAssetIdList.size(); i += multiAssetListSize) {
-      int toIndex = Math.min(i + multiAssetListSize, multiAssetIdList.size());
-      var subList = multiAssetIdList.subList(i, toIndex);
-      Long startIdent = subList.get(0);
-      Long endIdent = subList.get(subList.size() - 1);
+    for (int i = 0; i < numBatches; i++) {
 
-      tokenInfoFutures.add(
-          tokenInfoServiceAsync
-              .buildTokenInfoList(startIdent, endIdent, maxBlockNo, txId, updateTime)
+        var startIndex = (long) i * tokenInfoBatchSize + 1;
+        var toIndex = startIndex + tokenInfoBatchSize;
+        if (i == numBatches -1) {
+            toIndex += reminder;
+        }
+
+        if (i % 5 == 0) {
+            log.info("TokenInfoBatch - index: {}, start: {}, end: {}", i, startIndex, toIndex);
+        }
+
+
+          var tokenInfoList = tokenInfoServiceAsync
+              .buildTokenInfoList(startIndex, toIndex, maxBlockNo, txId, updateTime)
               .exceptionally(
                   e -> {
                     throw new RuntimeException(
                         "Exception occurs when initializing token info list", e);
-                  }));
+                  })
+                  .join();
+        jooqTokenInfoRepository.insertAll(tokenInfoList);
 
-      // After every 5 batches, insert the fetched token info data into the database in batches.
-      if (tokenInfoFutures.size() % 5 == 0) {
-        var tokenInfoList =
-            tokenInfoFutures.stream().map(CompletableFuture::join).flatMap(List::stream).toList();
-        BatchUtils.doBatching(1000, tokenInfoList, jooqTokenInfoRepository::insertAll);
-        tokenInfoFutures.clear();
-      }
     }
-
-    // Wait for the remaining CompletableFuture instances to complete.
-    CompletableFuture.allOf(tokenInfoFutures.toArray(new CompletableFuture[0])).join();
-    List<TokenInfo> tokenInfoList =
-        tokenInfoFutures.stream().map(CompletableFuture::join).flatMap(List::stream).toList();
-    BatchUtils.doBatching(1000, tokenInfoList, jooqTokenInfoRepository::insertAll);
-
-    multiAssetIdList.clear();
 
     tokenInfoCheckpointRepository.save(
         TokenInfoCheckpoint.builder().blockNo(maxBlockNo).updateTime(updateTime).build());
@@ -264,4 +257,11 @@ public class TokenInfoServiceImpl implements TokenInfoService {
   private String getRedisKey(String key) {
     return String.join("_", network.toUpperCase(), key);
   }
+
+
+    @PostConstruct
+    public void init(){
+        log.info("INIT - tokenInfoBatchSize: {}", tokenInfoBatchSize);
+    }
+
 }
