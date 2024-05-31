e634b6092fffd351beba3f1b8456d633234729b8
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-05-31 11:17:39 +0100

    chore: long shot rewriting the query

--- src/main/java/org/cardanofoundation/job/model/TokenVolume.java
+++ src/main/java/org/cardanofoundation/job/model/TokenVolume.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class TokenVolume {
+public class TokenVolume implements org.cardanofoundation.job.model.projection.TokenVolume {
   Long ident;
   BigInteger volume;
 
