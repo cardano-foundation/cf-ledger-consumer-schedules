9a793c7c491a399d8b9f5aebab956e05f362e0c8
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-07-02 19:02:27 +0700

    perf: trying to group token before fetch LatestTokenBalance

--- src/main/java/org/cardanofoundation/job/repository/ledgersync/TokenTxCountRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/ledgersync/TokenTxCountRepository.java
@@ -1,7 +1,14 @@
 package org.cardanofoundation.job.repository.ledgersync;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import org.cardanofoundation.explorer.common.entity.ledgersync.TokenTxCount;
 
-public interface TokenTxCountRepository extends JpaRepository<TokenTxCount, Long> {}
+public interface TokenTxCountRepository extends JpaRepository<TokenTxCount, String> {
+
+  List<TokenTxCount> findAllByUnitIn(@Param("units") Collection<String> units);
+}
