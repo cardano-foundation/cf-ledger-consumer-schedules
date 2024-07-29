0aca6ac47118ea966b5dd928303d41e50fd8ff4b
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-07-29 15:21:36 +0700

    feat: add composite index based on tx_hash column

--- src/main/resources/db/migration/ledgersyncagg/V1_4_6__drop_unused_indexes.sql
+++ src/main/resources/db/migration/ledgersyncagg/V1_4_6__drop_unused_indexes.sql
@@ -8,3 +8,12 @@ DROP INDEX IF EXISTS address_tx_amount_slot_idx;
 
 CREATE INDEX IF NOT EXISTS address_tx_amount_block_time_idx
     ON address_tx_amount (block_time);
+
+CREATE INDEX IF NOT EXISTS address_tx_amount_address_tx_hash_slot_index
+    ON address_tx_amount (address, tx_hash, slot);
+
+CREATE INDEX IF NOT EXISTS address_tx_amount_stake_address_tx_hash_slot_index
+    ON address_tx_amount (stake_address, tx_hash, slot);
+
+CREATE INDEX IF NOT EXISTS address_tx_amount_unit_tx_hash_slot_index
+    ON address_tx_amount (unit, tx_hash, slot);
