7dae7eddbabe2aeca58fd576a76537ba4b4c51f3
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-08-01 14:29:57 +0700

    chore: replace hard coded network value with constants

---
+++ src/main/java/org/cardanofoundation/job/common/constant/Constant.java
@@ -0,0 +1,12 @@
+package org.cardanofoundation.job.common.constant;
+
+public final class Constant {
+  private Constant() {}
+
+  public static final class NetworkType {
+    public static final String MAINNET = "mainnet";
+    public static final String PREVIEW = "preview";
+    public static final String PREPROD = "preprod";
+    public static final String SANCHONET = "sanchonet";
+  }
+}
