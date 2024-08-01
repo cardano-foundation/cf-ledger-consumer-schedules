fcb52c2c4996cdfddd9aa81c8b17a73b012b8c6c
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-08-01 16:39:10 +0700

    chore: remove unused

--- src/main/java/org/cardanofoundation/job/repository/ledgersync/OffChainVoteFetchErrorRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/ledgersync/OffChainVoteFetchErrorRepository.java
@@ -1,22 +1,9 @@
 package org.cardanofoundation.job.repository.ledgersync;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import org.cardanofoundation.explorer.common.entity.compositeKey.OffChainVoteFetchErrorId;
 import org.cardanofoundation.explorer.common.entity.ledgersync.OffChainVoteFetchError;
 
 public interface OffChainVoteFetchErrorRepository
-    extends JpaRepository<OffChainVoteFetchError, OffChainVoteFetchErrorId> {
-
-  @Query(
-      value =
-          """
-    SELECT e FROM OffChainVoteFetchError e WHERE e.retryCount <= :retryCount
-    AND NOT EXISTS (SELECT 1 FROM OffChainVoteGovActionData d WHERE d.anchorUrl = e.anchorUrl AND d.anchorHash = e.anchorHash)
-    """)
-  List<OffChainVoteFetchError> findByRetryCountLessThanEqual(@Param("retryCount") int retryCount);
-}
+    extends JpaRepository<OffChainVoteFetchError, OffChainVoteFetchErrorId> {}
