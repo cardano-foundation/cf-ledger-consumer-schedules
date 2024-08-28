03d536ca8274911fa728410c8472edc10e75f460
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-08-28 17:17:59 +0700

    chore: remove unused query

--- src/main/java/org/cardanofoundation/job/repository/ledgersyncagg/LatestTokenBalanceRepository.java
+++ src/main/java/org/cardanofoundation/job/repository/ledgersyncagg/LatestTokenBalanceRepository.java
@@ -19,24 +19,6 @@ public interface LatestTokenBalanceRepository
     extends JpaRepository<LatestTokenBalance, AddressBalanceId> {
 
   @Query(
-      value =
-          """
-          select max(ltb.block_time) from latest_token_balance ltb
-          where ltb.block_time != (select max(ltb2.block_time) from latest_token_balance ltb2)
-      """,
-      nativeQuery = true)
-  Long getTheSecondLastBlockTime();
-
-  @Query(
-      value =
-          """
-                  select max(ltb.slot) from latest_token_balance ltb
-                  where ltb.slot != (select max(ltb2.slot) from latest_token_balance ltb2)
-              """,
-      nativeQuery = true)
-  Long getTheSecondLastSlot();
-
-  @Query(
       """
           SELECT latestTokenBalance.policy as scriptHash, COALESCE(COUNT(latestTokenBalance), 0) as numberOfHolders
           FROM LatestTokenBalance latestTokenBalance
